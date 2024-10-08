b4a484395a3bde3137751179c2be95daa91ee896
anjosfelipe <felipebezerraanjos@gmail.com>
2024-10-08 11:09:20 -0300

    Add files via upload

---
+++ If.else.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int idadeFelipe = 17;
+    int idadeGeovanna = 16;
+
+    if(idadeFelipe > idadeGeovanna){
+        printf("Felipe e mais velho que Geovanna!");
+    }
+    else printf("Geovanna e mais velho que Felipe!");
+    
+}
